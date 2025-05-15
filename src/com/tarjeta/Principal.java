638045ed0110135edff8fcc475f106c82ae31c1c
Belen <belenbellini.94@gmail.com>
2025-05-14 21:06:48 -0300

    tarjeta

---
+++ src/com/tarjeta/Principal.java
@@ -0,0 +1,42 @@
+    package com.tarjeta;
+
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Principal {
+    public static void  main (String[] args){
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Escriba el límite de la tarjeta: ");
+        double limite = lectura.nextDouble();
+        TarjetaDeCredito tarjeta = new TarjetaDeCredito(limite);
+
+        int salir = 1;
+        while (salir != 9){
+            System.out.println("Escriba la descripción de la compra: ");
+            String descripcion = lectura.next();
+
+            System.out.println("Escriba el valor de la compra: ");
+            double valor = Double.valueOf(lectura.next());
+
+            Compra compra = new Compra(valor, descripcion);
+            boolean compraRealizada = tarjeta.hacerCompra(compra);
+
+            if (compraRealizada){
+                System.out.println("Compra realizada!");
+                System.out.println("Escribe 9 para salir o 1 para continuar");
+                salir = lectura.nextInt();
+            }else{
+                System.out.println("Saldo insuficiente!");
+                salir= 9;
+            }
+        }
+                System.out.println("**************************************");
+                System.out.println("COMPRAS REALIZADAS:\n");
+        Collections.sort(tarjeta.getListaDeCompras());
+                for (Compra compra: tarjeta.getListaDeCompras()){
+                    System.out.println(compra.getDescripcion() + " - " + compra.getValor());
+                }
+                System.out.println("\n**************************************");
+                System.out.println("\nSaldo de la tarjeta " + tarjeta.getSaldo());
+    }
+}
